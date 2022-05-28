6e0e065a09ea62c2f529286d56a3339f60d52cc4
Niket Sharma <niketsharma22@gmail.com>
2022-05-28 17:40:28 +0530

    Installed Express

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const http = require('http');
+const http = require("http");
 
-const routes = require('./routes');
-
-console.log(routes.someText);
-
-const server = http.createServer(routes.handler);
+const server = http.createServer((req, res) => {});
 
 server.listen(3000);
